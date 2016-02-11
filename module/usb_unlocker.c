e5ee2d31b605602718e866615751e6547cebce5f
cjackie <chaojie.keepgoing@gmail.com>
2016-02-11 09:27:27 -0800

    "timer" is working

--- module/usb_unlocker.c
+++ module/usb_unlocker.c
@@ -4,10 +4,12 @@
 
 #include <linux/module.h>
 #include <linux/kmod.h>
-#include <linux/timer.h>
 #include <linux/jiffies.h>
+#include <linux/wait.h>
+#include <linux/sched.h>
+#include <linux/workqueue.h>
 
-MODULE_LICENSE("MIT");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Chaojie Wang");
 
 #define CJ_DEBUG
@@ -21,75 +23,50 @@ struct unlocker_key {
 	int i;
 };
 
-#define NO_EVENT 0
-#define EVENT_PLUG 1
-#define EVENT_UNPULG 2
-#define STATUS_UNKNOWN 0
-#define STATUS_PLUG 1
-#define STATUS_UNPLUG 2
-/**
- * @event, if there is an event happened.
- * @status, current status.
- */
-struct timer_arg {
-	struct timer_list *timer;
-	wait_queue_head_t *wait;
-	struct unlocker_key key;
-	int event;
-	int status;
+static struct unlocker_key key = {
+	.i = 1,
 };
-static inline void clear_event(struct timer_arg *arg) {
-	arg->event = NO_EVENT;
-}
+static struct delayed_work usb_unlocker_work;
+
+extern struct workqueue_struct *system_long_wq; /* just use it */
 
 /**
  * walking over all usb devices, and see if a much
  */
-static void walk_usb_devices(unsigned long data) {
+static void walk_usb_devices(struct work_struct *work) {
 	printk(KERN_INFO "walk_usb_devices is invoked\n");
-	struct timer_arg *arg;
 
-	arg = (struct timer_arg *)data;
+	
 	/* walking over all */
+	/* and invoke helper function if needed */
 	/* TODO */
 	
-	mod_timer(arg->timer, TIMER_INTV);
-	add_timer(arg->timer);
+	if (!queue_delayed_work(system_long_wq, &usb_unlocker_work, TIMER_INTV)) {
+		printk(KERN_ERR "It is already on the queue?\n");
+	}
+	printk(KERN_INFO "key: %d\n", key.i);
 }
 
+
 static int __init usb_unlocker_init(void) {
-	wait_queue_head_t wait;
-	struct timer_arg timer_arg;
-	struct timer_list timer; 
+	printk(KERN_INFO "usb_unlocker_init is invoke\n");
 
-	init_waitqueue_head(&wait);
+	/* initial work */
+	INIT_DELAYED_WORK(&usb_unlocker_work, walk_usb_devices);
+  
+	/* invoke helper function to encrypt data */
+	/* TODO */
 
-	timer_arg.timer = &timer;
-	timer_arg.wait = &wait;
-	timer_arg.key.i = 1;
-	timer_arg.event = NO_EVENT;
-	timer_arg.status = STATUS_UNKNOWN;
-
-	setup_timer(&timer, walk_usb_devices, (unsigned long)&timer_arg)
-	add_timer(&timer);
-	
-	while (1) {
-		if (wait_event_interruptible(&wait, !timer_arg.event)) {
-			printk(KERN_ERR "being interrupted\n");
-		}
-
-		if (timer_arg.event) {
-			printk(KERN_INFO "An event happened\n");
-			clear_event(&timer_arg);
-		}
+	if (queue_delayed_work(system_long_wq, &usb_unlocker_work, TIMER_INTV)) {
+		printk(KERN_ERR "work is running?\n");
 	}
-
 	return 0;
 }
 
 	
 static void usb_unlocker_exit(void) {
-	
+	/* cancel all the work */
+	/* TODO */
 }
 
 module_init(usb_unlocker_init);
