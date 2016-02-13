f628a191ef175cce2d85c10c4b1632b743ea737d
cjackie <chaojie.keepgoing@gmail.com>
2016-02-13 12:40:09 -0800

    helper

---
+++ script/usb_unlocker_helper.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+static char *folder = "/home/chaojiewang/repos/usb-unlock/script";
+static int eflag, dflag;
+
+int main(int argc, char *argv[]) {
+  /* TODO */
+
+  exit(0);
+}
